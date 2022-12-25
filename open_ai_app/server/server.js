97f7c59ec748c6112bb14a13bd55f95b464116c3
Shahbab Ahmed <shahbaba@umich.edu>
2022-12-24 20:39:02 -0500

    setting up server.js with openai connection

---
+++ open_ai_app/server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIAPI } from 'openai';
+
+dotenv.config();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/',async (req,res) => {
+    res.status(200).send({message:"Hello from chatGPT"})
+})
+
+app.post('/', async (req,res) => {
+
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion()
+    } catch (error) {
+
+    }
+
+})
